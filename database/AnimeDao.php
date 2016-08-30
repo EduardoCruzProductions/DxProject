059a427fc53af710bebe45a96a5097e71f791271
Atrox_Portable <eduardo_acj2@hotmail.com>
2016-08-30 17:24:54 -0300

    Implementacao do DAO

---
+++ database/AnimeDao.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once "../entidades/animes.php";
+include_once "Conexao.php";
+
+class AnimeDao {
+    
+    public function salvar(Anime $a){
+        
+        try{
+            
+            $con = Conexao::get();
+            $stmt = $con->prepare('INSERT INTO anime(id_publico,nome,n_episodios,sinopse,genero,img) VALUES (?,?,?,?,?,?)');
+            $stmt->bind_param('isssss',$id_publico,$nome,$n_episodios,$sinopse,$genero,$img);
+            $id_publico = $a->getId_publico();
+            $nome = $a->getNome();
+            $n_episodios = $a->getN_episodios();
+            $sinopse = $a->getSinopse();
+            $genero = $a->getGenero();
+            $img = $a->getImg();
+            $stmt->execute();
+            $a->setId($con->insert_id);
+            
+        }catch(Exception $e){
+            print($e);
+        }
+        
+    }
+    
+    
+    
+}
